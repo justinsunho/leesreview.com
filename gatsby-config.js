5e95b553dab27255570508b3bbf2b0a112cacc31
Justin Kim <justinkim7395@gmail.com>
2021-01-04 13:20:34 -0800

    Initial Configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,56 @@
 module.exports = {
-  siteMetadata: {
-    title: "leesreview.com",
-  },
-  plugins: [
-    "gatsby-plugin-netlify-cms",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
-    "gatsby-plugin-mdx",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
+    siteMetadata: {
+        title: `Lees Review`,
+        author: `Justin Sunho Kim`,
+        description: `Lees Review Tutoring Center`,
+        siteUrl: `https://leesreview.com`,
     },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+    plugins: [
+        "gatsby-plugin-netlify-cms",
+        "gatsby-plugin-sass",
+        "gatsby-plugin-sharp",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-offline",
+        "gatsby-plugin-mdx",
+        "gatsby-transformer-sharp",
+        `gatsby-plugin-resolve-src`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/static/images`,
+                name: `assets`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/content/staff`,
+                name: `staff`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/content/testimonies`,
+                name: `testimonies`,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-relative-images`,
+                        options: {
+                            name: "assets",
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-images`,
+                    },
+                ],
+            },
+        },
+    ],
 };
