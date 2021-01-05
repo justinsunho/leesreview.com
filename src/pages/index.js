7812c88fe2c60f657cefdabeee254f9986a7d78c
Justin Kim <justinkim7395@gmail.com>
2021-01-04 18:01:45 -0800

    Home Page Structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { graphql } from "gatsby";
 
-const Index = () => {
-    return <div>Home</div>;
+const Index = ({ data }) => {
+    const {
+        allPagesJson: { edges },
+    } = data;
+
+    const {
+        node: { title, hero },
+    } = edges[0];
+
+    return <div>{title}</div>;
 };
 
 export default Index;
+
+export const pageQuery = graphql`
+    query {
+        allPagesJson(filter: { title: { eq: "Home" } }) {
+            edges {
+                node {
+                    id
+                    hero {
+                        staff
+                        description
+                        buttonText
+                        buttonLink
+                        image
+                    }
+                    title
+                }
+            }
+        }
+    }
+`;
