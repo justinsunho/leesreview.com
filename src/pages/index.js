cd221cd680dedb82c050a24142dbff66347a8246
Justin Kim <justinkim7395@gmail.com>
2021-01-06 14:24:45 -0800

    Temp

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,17 @@ import { Hero } from "components/organisms";
 import { MainLayout } from "components/layouts";
 
 const Index = ({ data }) => {
-    const {
-        allPagesJson: { edges },
-    } = data;
+    // const {
+    //     allMarkdownRemark: { edges },
+    // } = data;
 
-    const {
-        node: { hero, classes },
-    } = edges[0];
+    // const {
+    //     node: { hero, classes },
+    // } = edges[0];
 
     return (
         <MainLayout>
-            <Hero
+            {/* <Hero
                 className={"section"}
                 headingText={hero.title}
                 description={hero.description}
@@ -38,41 +38,41 @@ const Index = ({ data }) => {
                         />
                     ))}
                 </div>
-            </div>
+            </div> */}
         </MainLayout>
     );
 };
 
 export default Index;
 
-export const pageQuery = graphql`
-    query {
-        allPagesJson(filter: { title: { eq: "Home" } }) {
-            edges {
-                node {
-                    title
-                    id
-                    hero {
-                        title
-                        description
-                        buttonText
-                        buttonLink
-                        image
-                    }
-                    classes {
-                        title
-                        linkText
-                        linkHref
-                        classList {
-                            title
-                            description
-                            linkText
-                            linkHref
-                            image
-                        }
-                    }
-                }
-            }
-        }
-    }
-`;
+// export const pageQuery = graphql`
+//     query {
+//         allMarkdownRemark(filter: { title: { eq: "Home" } }) {
+//             edges {
+//                 node {
+//                     title
+//                     id
+//                     hero {
+//                         title
+//                         description
+//                         buttonText
+//                         buttonLink
+//                         image
+//                     }
+//                     classes {
+//                         title
+//                         linkText
+//                         linkHref
+//                         classList {
+//                             title
+//                             description
+//                             linkText
+//                             linkHref
+//                             image
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// `;
