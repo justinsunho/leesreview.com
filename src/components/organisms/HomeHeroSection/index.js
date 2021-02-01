99702632654f60de90a2b488d5ea9497c6fc03c9
Justin Kim <justinkim7395@gmail.com>
2021-02-01 13:28:20 -0800

    Home experiments with bubble animation

--- src/components/organisms/HomeHeroSection/index.js
+++ src/components/organisms/HomeHeroSection/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useInView } from "react-intersection-observer";
 import Img from "gatsby-image";
-import { a, useSpring } from "react-spring";
+import { a, useSpring, useSprings, interpolate } from "react-spring";
 import { Image } from "components/atoms";
 import { TextBlock } from "components/molecules";
 import { enterLeft } from "utilities/springConfigs";
@@ -14,16 +14,56 @@ const HomeHeroSection = ({ headingText, description, linkText, linkHref, button,
         triggerOnce: true,
     });
 
-    const AnimatedImage = a(Image);
+    const AnimatedImage = a(Img);
 
     const imageSpring = useSpring({
         ...enterLeft(inView),
     });
 
+    const blah = [0, 1, 2, 3];
+
+    const springs = useSprings(
+        blah.length,
+        blah.map((item, i) => ({
+            from: {
+                top: 0,
+                left: `${-2000 + i * 30}px`,
+                width: `6rem`,
+                height: `6rem`,
+            },
+            to: {
+                left: `${window.innerWidth + 200}px`,
+                top: 1,
+            },
+            config: {
+                duration: `${i * 10000}`,
+            },
+            loop: true,
+        }))
+    );
+
     return (
         <div className={`section ${styles.homeHeroWrapper}`} ref={ref}>
             <div className={`${styles.hero} row justify-content-center ${className}`}>
-                <Img className={`${styles.image}`} fluid={image} style={{ position: "absolute" }} />
+                {springs.map((props) => (
+                    <AnimatedImage
+                        className={`${styles.image}`}
+                        fluid={image}
+                        style={{
+                            position: "absolute",
+                            top: props.top
+                                .interpolate({
+                                    range: [0, 0.75, 1],
+                                    output: [200, 100, 200],
+                                })
+                                .interpolate((top) => `${top}px`),
+                            left: props.left,
+                            width: props.width,
+                            height: props.height,
+                        }}
+                    />
+                ))}
+
                 <TextBlock
                     className={`col-md-7 align-items-center justify-content-center ${utilities.textCenter}`}
                     headingTag={"h1"}
@@ -34,7 +74,6 @@ const HomeHeroSection = ({ headingText, description, linkText, linkHref, button,
                     linkHref={linkHref}
                     button={button}
                     inView={inView}
-                    darkMode
                 />
 
                 {/* <AnimatedImage className={} image={image} style={imageSpring} /> */}
