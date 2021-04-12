8969b9153f2e8951fe879c669743c78959654363
Christine Yoo <christinecho.s@gmail.com>
2021-04-11 22:23:36 -0700

    Changed Lees to Lee's
    Fixes issue #34 (title tag in gatsby-config.js)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,79 +1,79 @@
 module.exports = {
-    siteMetadata: {
-        title: `Lees Review`,
-        author: `Justin Sunho Kim`,
-        description: `Lees Review Tutoring Center. SAT Prepration and College Consulting services.`,
-        siteUrl: `http://leesreview.com`,
-        image: "/images/logo.jpg",
+  siteMetadata: {
+    title: `Lee's Review`,
+    author: `Justin Sunho Kim`,
+    description: `Lee's Review Tutoring Center. SAT Prepration and College Consulting services.`,
+    siteUrl: `http://leesreview.com`,
+    image: '/images/logo.jpg'
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/images`,
+        name: `images`
+      }
     },
-    plugins: [
-        {
-            resolve: `gatsby-source-filesystem`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/staff`,
+        name: `staff`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/testimonies`,
+        name: `testimonies`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/schedules`,
+        name: `schedules`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/classes`,
+        name: `classes`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/pages`,
+        name: `pages`
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-relative-images`,
             options: {
-                path: `${__dirname}/static/images`,
-                name: `images`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/content/staff`,
-                name: `staff`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/content/testimonies`,
-                name: `testimonies`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/content/schedules`,
-                name: `schedules`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/content/classes`,
-                name: `classes`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/content/pages`,
-                name: `pages`,
-            },
-        },
-        "gatsby-transformer-sharp",
-        "gatsby-plugin-sharp",
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-relative-images`,
-                        options: {
-                            name: "images",
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-images`,
-                    },
-                ],
-            },
-        },
-        "gatsby-plugin-image",
-        "gatsby-plugin-netlify-cms",
-        "gatsby-plugin-sass",
-        "gatsby-plugin-react-helmet",
-        "gatsby-plugin-sitemap",
-        "gatsby-plugin-offline",
-        `gatsby-plugin-resolve-src`,
-        "gatsby-transformer-json",
-    ],
+              name: 'images'
+            }
+          },
+          {
+            resolve: `gatsby-remark-images`
+          }
+        ]
+      }
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
+    `gatsby-plugin-resolve-src`,
+    'gatsby-transformer-json'
+  ]
 };
