12dcbf706b386e8d6d53532c58a52b11ebf46166
Christine Yoo <christinecho.s@gmail.com>
2021-04-27 18:40:52 -0700

    Removed price field in the pricing cards
    Almost fixes issue #8

--- src/components/molecules/ClassCard/index.js
+++ src/components/molecules/ClassCard/index.js
@@ -1,30 +1,42 @@
-import React from "react";
-import { Link } from "gatsby";
-import * as styles from "./styles.module.scss";
+import React from 'react';
+import { Link } from 'gatsby';
+import * as styles from './styles.module.scss';
 
-const ClassCard = ({ backgroundColor, date, description, price, teacherLink, teacherName, time, title }) => {
-    return (
-        <div className={`${styles.classCardContainer}`}>
-            <div className={`${styles.classCardWrapper}`}>
-                <h4 className={`${styles.heading}`}>{title}</h4>
-                {date && (
-                    <div className={`${styles.dateContainer}`}>
-                        <div>{date}</div>
-                        <div>{time}</div>
-                    </div>
-                )}
-                <p className={styles.description} dangerouslySetInnerHTML={{ __html: description }} />
-                {teacherName && (
-                    <Link className={`${styles.teacher}`} to={teacherLink}>
-                        Taught by: {teacherName}
-                    </Link>
-                )}
-            </div>
-            <div className={`${styles.price}`} style={{ backgroundColor: backgroundColor }}>
-                {price}
-            </div>
-        </div>
-    );
+const ClassCard = ({
+  backgroundColor,
+  date,
+  description,
+  teacherLink,
+  teacherName,
+  time,
+  title
+}) => {
+  return (
+    <div className={`${styles.classCardContainer}`}>
+      <div className={`${styles.classCardWrapper}`}>
+        <h4 className={`${styles.heading}`}>{title}</h4>
+        {date && (
+          <div className={`${styles.dateContainer}`}>
+            <div>{date}</div>
+            <div>{time}</div>
+          </div>
+        )}
+        <p
+          className={styles.description}
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
+        {teacherName && (
+          <Link className={`${styles.teacher}`} to={teacherLink}>
+            Taught by: {teacherName}
+          </Link>
+        )}
+      </div>
+      <div
+        className={`${styles.price}`}
+        style={{ backgroundColor: backgroundColor }}
+      ></div>
+    </div>
+  );
 };
 
 export default ClassCard;
