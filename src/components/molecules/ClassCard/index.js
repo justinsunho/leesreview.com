4ba9ef454198bde54dd8ceef6d1c37ce35371726
Justin Kim <justinkim7395@gmail.com>
2021-05-09 22:42:33 -0700

    Remove unecessary div

--- src/components/molecules/ClassCard/index.js
+++ src/components/molecules/ClassCard/index.js
@@ -1,42 +1,27 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import * as styles from './styles.module.scss';
+import React from "react";
+import { Link } from "gatsby";
+import * as styles from "./styles.module.scss";
 
-const ClassCard = ({
-  backgroundColor,
-  date,
-  description,
-  teacherLink,
-  teacherName,
-  time,
-  title
-}) => {
-  return (
-    <div className={`${styles.classCardContainer}`}>
-      <div className={`${styles.classCardWrapper}`}>
-        <h4 className={`${styles.heading}`}>{title}</h4>
-        {date && (
-          <div className={`${styles.dateContainer}`}>
-            <div>{date}</div>
-            <div>{time}</div>
-          </div>
-        )}
-        <p
-          className={styles.description}
-          dangerouslySetInnerHTML={{ __html: description }}
-        />
-        {teacherName && (
-          <Link className={`${styles.teacher}`} to={teacherLink}>
-            Taught by: {teacherName}
-          </Link>
-        )}
-      </div>
-      <div
-        className={`${styles.bottomTab}`}
-        style={{ backgroundColor: backgroundColor }}
-      ></div>
-    </div>
-  );
+const ClassCard = ({ backgroundColor, date, description, teacherLink, teacherName, time, title }) => {
+    return (
+        <div className={`${styles.classCardContainer}`} style={{ borderBottomColor: backgroundColor }}>
+            <div className={`${styles.classCardWrapper}`}>
+                <h4 className={`${styles.heading}`}>{title}</h4>
+                {date && (
+                    <div className={`${styles.dateContainer}`}>
+                        <div>{date}</div>
+                        <div>{time}</div>
+                    </div>
+                )}
+                <p className={styles.description} dangerouslySetInnerHTML={{ __html: description }} />
+                {teacherName && (
+                    <Link className={`${styles.teacher}`} to={teacherLink}>
+                        Taught by: {teacherName}
+                    </Link>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default ClassCard;
