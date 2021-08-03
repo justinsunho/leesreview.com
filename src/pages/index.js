9a7db6e7fca4a4fb26d0704e59e2a66655f5abe2
Justin Kim <justinkim7395@gmail.com>
2021-08-03 13:23:40 -0700

    Remove faulty link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,149 +1,160 @@
 import React from "react";
 import { graphql } from "gatsby";
-import { HomeHeroSection, MediaSection, TestimonySection, LinkCardContainer } from "components/organisms";
+import {
+  HomeHeroSection,
+  MediaSection,
+  TestimonySection,
+  LinkCardContainer
+} from "components/organisms";
 import { MainLayout } from "components/layouts";
 import * as styles from "./styles.module.scss";
 
 const Index = ({ data }) => {
-    const {
-        page: { edges: pageEdges },
-        testimonies: { edges: testimonyList },
-    } = data;
+  const {
+    page: { edges: pageEdges },
+    testimonies: { edges: testimonyList }
+  } = data;
 
-    const {
-        node: {
-            frontmatter: { about, classes, hero, location, testimonies },
-        },
-    } = pageEdges[0];
+  const {
+    node: {
+      frontmatter: { about, classes, hero, location, testimonies }
+    }
+  } = pageEdges[0];
 
-    return (
-        <MainLayout bannerDarkMode className={styles.pageGradient}>
-            <HomeHeroSection
-                bigButton
-                button
-                description={hero.description}
-                headingText={hero.title}
-                image={hero.image.childImageSharp.gatsbyImageData}
-                linkHref={hero.buttonLink}
-                linkText={hero.buttonText}
-            />
-            <LinkCardContainer
-                backgroundClassName={styles.linkCardBackground}
-                items={classes.classList}
-                linkHref={classes.linkHref}
-                linkText={classes.linkText}
-                title={classes.title}
-            />
-            <MediaSection
-                description={about.description}
-                headingText={about.title}
-                image={about.image.childImageSharp.gatsbyImageData}
-                linkHref={about.linkHref}
-                linkText={about.linkText}
-                ribbon={true}
-                subtitle={about.subtitle}
-                videoId={"QRGoswZh6fA"}
-            />
-            <TestimonySection
-                linkHref={testimonies.linkHref}
-                linkText={testimonies.linkText}
-                subtitle={testimonies.subtitle}
-                testimonyList={testimonyList}
-            />
-            <LinkCardContainer items={location.locationList} subtitle={location.subtitle} title={location.title} />
-        </MainLayout>
-    );
+  return (
+    <MainLayout bannerDarkMode className={styles.pageGradient}>
+      <HomeHeroSection
+        bigButton
+        button
+        description={hero.description}
+        headingText={hero.title}
+        image={hero.image.childImageSharp.gatsbyImageData}
+        linkHref={hero.buttonLink}
+        linkText={hero.buttonText}
+      />
+      <LinkCardContainer
+        backgroundClassName={styles.linkCardBackground}
+        items={classes.classList}
+        title={classes.title}
+      />
+      <MediaSection
+        description={about.description}
+        headingText={about.title}
+        image={about.image.childImageSharp.gatsbyImageData}
+        linkHref={about.linkHref}
+        linkText={about.linkText}
+        ribbon={true}
+        subtitle={about.subtitle}
+        videoId={"QRGoswZh6fA"}
+      />
+      <TestimonySection
+        linkHref={testimonies.linkHref}
+        linkText={testimonies.linkText}
+        subtitle={testimonies.subtitle}
+        testimonyList={testimonyList}
+      />
+      <LinkCardContainer
+        items={location.locationList}
+        subtitle={location.subtitle}
+        title={location.title}
+      />
+    </MainLayout>
+  );
 };
 
 export default Index;
 
 export const pageQuery = graphql`
-    {
-        page: allMarkdownRemark(filter: { frontmatter: { title: { eq: "Home" } } }) {
-            edges {
-                node {
-                    frontmatter {
-                        hero {
-                            title
-                            description
-                            buttonText
-                            buttonLink
-                            staff
-                            image {
-                                childImageSharp {
-                                    gatsbyImageData(layout: FULL_WIDTH)
-                                }
-                            }
-                        }
-                        classes {
-                            title
-                            linkText
-                            linkHref
-                            classList {
-                                title
-                                image {
-                                    childImageSharp {
-                                        gatsbyImageData(layout: FULL_WIDTH)
-                                    }
-                                }
-                                description
-                                linkText
-                                linkHref
-                            }
-                        }
-                        about {
-                            image {
-                                childImageSharp {
-                                    gatsbyImageData(layout: FULL_WIDTH)
-                                }
-                            }
-                            subtitle
-                            title
-                            description
-                            linkText
-                            linkHref
-                        }
-                        testimonies {
-                            subtitle
-                            linkText
-                            linkHref
-                        }
-                        location {
-                            subtitle
-                            title
-                            locationList {
-                                title
-                                description
-                                linkText
-                                linkHref
-                                image {
-                                    childImageSharp {
-                                        gatsbyImageData(layout: FULL_WIDTH)
-                                    }
-                                }
-                            }
-                        }
-                    }
+  {
+    page: allMarkdownRemark(
+      filter: { frontmatter: { title: { eq: "Home" } } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            hero {
+              title
+              description
+              buttonText
+              buttonLink
+              staff
+              image {
+                childImageSharp {
+                  gatsbyImageData(layout: FULL_WIDTH)
                 }
+              }
             }
-        }
-        testimonies: allMarkdownRemark(filter: { frontmatter: { featured: { eq: "Home" } } }) {
-            edges {
-                node {
-                    id
-                    frontmatter {
-                        title
-                        college
-                        tags
-                        image {
-                            childImageSharp {
-                                gatsbyImageData(layout: FULL_WIDTH)
-                            }
-                        }
-                    }
-                    rawMarkdownBody
+            classes {
+              title
+              linkText
+              linkHref
+              classList {
+                title
+                image {
+                  childImageSharp {
+                    gatsbyImageData(layout: FULL_WIDTH)
+                  }
                 }
+                description
+                linkText
+                linkHref
+              }
             }
+            about {
+              image {
+                childImageSharp {
+                  gatsbyImageData(layout: FULL_WIDTH)
+                }
+              }
+              subtitle
+              title
+              description
+              linkText
+              linkHref
+            }
+            testimonies {
+              subtitle
+              linkText
+              linkHref
+            }
+            location {
+              subtitle
+              title
+              locationList {
+                title
+                description
+                linkText
+                linkHref
+                image {
+                  childImageSharp {
+                    gatsbyImageData(layout: FULL_WIDTH)
+                  }
+                }
+              }
+            }
+          }
         }
+      }
     }
+    testimonies: allMarkdownRemark(
+      filter: { frontmatter: { featured: { eq: "Home" } } }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            college
+            tags
+            image {
+              childImageSharp {
+                gatsbyImageData(layout: FULL_WIDTH)
+              }
+            }
+          }
+          rawMarkdownBody
+        }
+      }
+    }
+  }
 `;
