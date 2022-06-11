af50e16feef915f3e073e9cb8358e2f755943145
Justin Kim <justinkim7395@gmail.com>
2022-06-11 13:08:43 -0700

    map

--- src/components/molecules/Tile/index.js
+++ src/components/molecules/Tile/index.js
@@ -2,26 +2,50 @@ import React from "react";
 import { EmbedVideo } from "components/atoms";
 import * as styles from "./styles.module.scss";
 
-const Tile = ({ className, color, description, icon, style, title, videoId }) => {
-    return (
-        <div className={`${className} ${styles.tileContainer}`} style={{ ...style, borderLeft: `8px solid ${color}` }}>
-            {videoId ? (
-                <EmbedVideo alt={title} className={styles.videoContainer} videoId={videoId} />
-            ) : (
-                icon && (
-                    <div
-                        className={styles.icon}
-                        dangerouslySetInnerHTML={{ __html: icon }}
-                        style={{ backgroundColor: `${color}` }}
-                    />
-                )
-            )}
-            <div className={`${styles.container} ${(icon || videoId) && styles.topItem}`}>
-                <h4 className={styles.heading}>{title}</h4>
-                <p className={styles.description} dangerouslySetInnerHTML={{ __html: description }} />
-            </div>
-        </div>
-    );
+const Tile = ({
+  className,
+  color,
+  description,
+  icon,
+  style,
+  title,
+  videoId,
+  map
+}) => {
+  return (
+    <div
+      className={`${className} ${styles.tileContainer}`}
+      style={{ ...style, borderLeft: `8px solid ${color}` }}
+    >
+      {videoId ? (
+        <EmbedVideo
+          alt={title}
+          className={styles.videoContainer}
+          videoId={videoId}
+        />
+      ) : (
+        icon && (
+          <div
+            className={styles.icon}
+            dangerouslySetInnerHTML={{ __html: icon }}
+            style={{ backgroundColor: `${color}` }}
+          />
+        )
+      )}
+      {map && (
+        <div className={styles.map} dangerouslySetInnerHTML={{ __html: map }} />
+      )}
+      <div
+        className={`${styles.container} ${(icon || videoId) && styles.topItem}`}
+      >
+        <h4 className={styles.heading}>{title}</h4>
+        <p
+          className={styles.description}
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Tile;
--- src/components/organisms/TileContainer/index.js
+++ src/components/organisms/TileContainer/index.js
@@ -7,50 +7,55 @@ import { colorArray } from "utilities/colorArray";
 import * as styles from "./styles.module.scss";
 
 const TileContainer = ({ backgroundClassName, className, items, title }) => {
-    const { inView, ref } = useInView({
-        threshold: 0.2,
-        triggerOnce: true,
-    });
+  const { inView, ref } = useInView({
+    threshold: 0.2,
+    triggerOnce: true
+  });
 
-    const textRef = useRef();
-    const textTrail = useTrail(2, {
-        ...enterAbove(inView),
-        config: { mass: 5, tension: 2000, friction: 200 },
-        ref: textRef,
-    });
+  const textRef = useRef();
+  const textTrail = useTrail(2, {
+    ...enterAbove(inView),
+    config: { mass: 5, tension: 2000, friction: 200 },
+    ref: textRef
+  });
 
-    const tileRef = useRef();
-    const tileTrail = useTrail(items.length, {
-        ...enterAbove(inView),
-        config: { mass: 15, tension: 2000, friction: 200 },
-        ref: tileRef,
-    });
+  const tileRef = useRef();
+  const tileTrail = useTrail(items.length, {
+    ...enterAbove(inView),
+    config: { mass: 15, tension: 2000, friction: 200 },
+    ref: tileRef
+  });
 
-    useChain(inView ? [textRef, tileRef] : []);
+  useChain(inView ? [textRef, tileRef] : []);
 
-    return (
-        <SectionWrapper
-            backgroundClassName={`${backgroundClassName} ${className}`}
-            className={` ${styles.tileBlockSection}`}
-            ref={ref}
-            title={title}
-            trailArray={textTrail}
-        >
-            <div className={`row ${styles.tileContainer}`}>
-                {items.map((item, i) => (
-                    <a.div className={`col-lg col-md-6 ${styles.tileWrapper}`} key={i} style={tileTrail[i]}>
-                        <Tile
-                            color={colorArray[i]}
-                            description={item.description}
-                            icon={item.icon && item.icon.code}
-                            title={item.title}
-                            videoId={item.videoId && item.videoId}
-                        />
-                    </a.div>
-                ))}
-            </div>
-        </SectionWrapper>
-    );
+  return (
+    <SectionWrapper
+      backgroundClassName={`${backgroundClassName} ${className}`}
+      className={` ${styles.tileBlockSection}`}
+      ref={ref}
+      title={title}
+      trailArray={textTrail}
+    >
+      <div className={`row ${styles.tileContainer}`}>
+        {items.map((item, i) => (
+          <a.div
+            className={`col-lg col-md-6 ${styles.tileWrapper}`}
+            key={i}
+            style={tileTrail[i]}
+          >
+            <Tile
+              color={colorArray[i]}
+              description={item.description}
+              icon={item.icon && item.icon.code}
+              map={item.map && item.map}
+              title={item.title}
+              videoId={item.videoId && item.videoId}
+            />
+          </a.div>
+        ))}
+      </div>
+    </SectionWrapper>
+  );
 };
 
 export default TileContainer;
