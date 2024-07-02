3ca86d64c21bbde62a14d23d40c7e93500c70f25
Justin Kim <justinkim7395@gmail.com>
2024-07-01 19:13:56 -0700

    test

--- src/pages/college-consulting/index.js
+++ src/pages/college-consulting/index.js
@@ -1,176 +1,206 @@
 import React from "react";
 import { graphql } from "gatsby";
 import {
-  Hero,
-  TileContainer,
-  MediaSection,
-  MethodsContainer,
-  TestimonySection,
-  PriceCardContainer,
-  BottomSection
+    Hero,
+    TileContainer,
+    MediaSection,
+    MethodsContainer,
+    TestimonySection,
+    ScheduleSection,
+    BottomSection
 } from "components/organisms";
 import { MainLayout } from "components/layouts";
 import * as styles from "./styles.module.scss";
 
 const CollegeConsulting = ({ data }) => {
-  const {
-    page: { edges: pageEdges },
-    testimonies: { edges: testimonyList }
-  } = data;
+    const {
+        page: { edges: pageEdges },
+        testimonies: { edges: testimonyList }
+    } = data;
 
-  const {
-    node: {
-      frontmatter: {
-        hero,
-        mediaSection,
-        methodsList,
-        priceList,
-        servicesList,
-        signUp,
-        testimonies
-      }
-    }
-  } = pageEdges[0];
+    const {
+        node: {
+            frontmatter: {
+                hero,
+                mediaSection,
+                methodsList,
+                priceList,
+                servicesList,
+                signUp,
+                testimonies
+            }
+        }
+    } = pageEdges[0];
 
-  return (
-    <MainLayout>
-      <Hero
-        description={hero.description}
-        headingText={hero.title}
-        image={hero.image.childImageSharp.gatsbyImageData}
-        linkHref={hero.buttonLink}
-        linkText={hero.buttonText}
-        subtitle={hero.subtitle}
-      />
-      <TileContainer items={servicesList} title={`Our College Consulting`} />
-      <MediaSection
-        description={mediaSection.description}
-        headingText={mediaSection.title}
-        image={mediaSection.image.childImageSharp.gatsbyImageData}
-        linkHref={mediaSection.linkHref}
-        linkText={mediaSection.linkText}
-        ribbon={true}
-        subtitle={mediaSection.subtitle}
-        videoId={mediaSection.videoId}
-      />
-      <MethodsContainer
-        backgroundClassName={styles.methodBackgroundClassName}
-        items={methodsList}
-        title={`Our Methods`}
-      />
-      <TestimonySection
-        linkHref={testimonies.linkHref}
-        linkText={testimonies.linkText}
-        subtitle={testimonies.subtitle}
-        testimonyList={testimonyList}
-      />
-      {/* <PriceCardContainer
+    return (
+        <MainLayout>
+            <Hero
+                description={hero.description}
+                headingText={hero.title}
+                image={hero.image.childImageSharp.gatsbyImageData}
+                linkHref={hero.buttonLink}
+                linkText={hero.buttonText}
+                subtitle={hero.subtitle}
+            />
+            <ScheduleSection
+                description={
+                    <>
+                        "Our College Consulting helps guide you through the
+                        logistics of the college application process so that you
+                        can have more time to grow into your potential. We love
+                        engaging with our students through the application
+                        process in order to help them gain new insights into who
+                        they are."
+                        <iframe
+                            src="https://docs.google.com/presentation/d/e/2PACX-1vQGRr6eJPVt1DpJft-BnCh2XDqomi3C1MjhxYLlOTiZ5lf2TwZF4SbR9DquCRZeymgzh7Ka5XBIrwSP/embed?start=false&amp;loop=false&amp;delayms=5000"
+                            frameborder="0"
+                            width="960"
+                            height="569"
+                            allowfullscreen="true"
+                            mozallowfullscreen="true"
+                            webkitallowfullscreen="true"
+                        ></iframe>
+                    </>
+                }
+                linkHref="https://drive.google.com/file/d/1SFkYTOzu0_n3Nsi0b3IOgVzzIwnxThZ5/view?usp=sharing"
+                linkText={
+                    "Click here to see our College Consulting program brochure"
+                }
+                title={"College Consulting Program"}
+            />
+            <TileContainer
+                items={servicesList}
+                title={`Our College Consulting`}
+            />
+            <MediaSection
+                description={mediaSection.description}
+                headingText={mediaSection.title}
+                image={mediaSection.image.childImageSharp.gatsbyImageData}
+                linkHref={mediaSection.linkHref}
+                linkText={mediaSection.linkText}
+                ribbon={true}
+                subtitle={mediaSection.subtitle}
+                videoId={mediaSection.videoId}
+            />
+
+            <MethodsContainer
+                backgroundClassName={styles.methodBackgroundClassName}
+                items={methodsList}
+                title={`Our Methods`}
+            />
+            <TestimonySection
+                linkHref={testimonies.linkHref}
+                linkText={testimonies.linkText}
+                subtitle={testimonies.subtitle}
+                testimonyList={testimonyList}
+            />
+            {/* <PriceCardContainer
                 backgroundClassName={styles.priceCardBackgroundClassName}
                 items={priceList}
                 title={`Our Packages`}
             /> */}
-      <BottomSection
-        buttonLink={signUp.buttonLink}
-        buttonText={signUp.buttonText}
-        description={signUp.description}
-        subtitle={signUp.subtitle}
-        title={signUp.title}
-      />
-    </MainLayout>
-  );
+            <BottomSection
+                buttonLink={signUp.buttonLink}
+                buttonText={signUp.buttonText}
+                description={signUp.description}
+                subtitle={signUp.subtitle}
+                title={signUp.title}
+            />
+        </MainLayout>
+    );
 };
 
 export default CollegeConsulting;
 
 export const pageQuery = graphql`
-  {
-    page: allMarkdownRemark(
-      filter: { frontmatter: { title: { eq: "College Consulting" } } }
-    ) {
-      edges {
-        node {
-          frontmatter {
-            title
-            hero {
-              title
-              subtitle
-              description
-              buttonText
-              buttonLink
-              image {
-                childImageSharp {
-                  gatsbyImageData(layout: FULL_WIDTH)
+    {
+        page: allMarkdownRemark(
+            filter: { frontmatter: { title: { eq: "College Consulting" } } }
+        ) {
+            edges {
+                node {
+                    frontmatter {
+                        title
+                        hero {
+                            title
+                            subtitle
+                            description
+                            buttonText
+                            buttonLink
+                            image {
+                                childImageSharp {
+                                    gatsbyImageData(layout: FULL_WIDTH)
+                                }
+                            }
+                        }
+                        servicesList {
+                            title
+                            description
+                            icon {
+                                code
+                            }
+                        }
+                        mediaSection {
+                            title
+                            subtitle
+                            description
+                            linkText
+                            linkHref
+                            videoId
+                            image {
+                                childImageSharp {
+                                    gatsbyImageData(layout: FULL_WIDTH)
+                                }
+                            }
+                        }
+                        methodsList {
+                            title
+                            description
+                            image {
+                                childImageSharp {
+                                    gatsbyImageData(layout: FULL_WIDTH)
+                                }
+                            }
+                        }
+                        testimonies {
+                            subtitle
+                            linkText
+                            linkHref
+                        }
+                        priceList {
+                            title
+                            description
+                        }
+                        signUp {
+                            subtitle
+                            title
+                            description
+                            buttonText
+                            buttonLink
+                        }
+                    }
                 }
-              }
             }
-            servicesList {
-              title
-              description
-              icon {
-                code
-              }
-            }
-            mediaSection {
-              title
-              subtitle
-              description
-              linkText
-              linkHref
-              videoId
-              image {
-                childImageSharp {
-                  gatsbyImageData(layout: FULL_WIDTH)
-                }
-              }
-            }
-            methodsList {
-              title
-              description
-              image {
-                childImageSharp {
-                  gatsbyImageData(layout: FULL_WIDTH)
-                }
-              }
-            }
-            testimonies {
-              subtitle
-              linkText
-              linkHref
-            }
-            priceList {
-              title
-              description
-            }
-            signUp {
-              subtitle
-              title
-              description
-              buttonText
-              buttonLink
-            }
-          }
         }
-      }
-    }
-    testimonies: allMarkdownRemark(
-      filter: { frontmatter: { featured: { eq: "College Consulting" } } }
-    ) {
-      edges {
-        node {
-          frontmatter {
-            title
-            college
-            tags
-            videoId
-            image {
-              childImageSharp {
-                gatsbyImageData(layout: FULL_WIDTH)
-              }
+        testimonies: allMarkdownRemark(
+            filter: { frontmatter: { featured: { eq: "College Consulting" } } }
+        ) {
+            edges {
+                node {
+                    frontmatter {
+                        title
+                        college
+                        tags
+                        videoId
+                        image {
+                            childImageSharp {
+                                gatsbyImageData(layout: FULL_WIDTH)
+                            }
+                        }
+                    }
+                    rawMarkdownBody
+                }
             }
-          }
-          rawMarkdownBody
         }
-      }
     }
-  }
 `;
